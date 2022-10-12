0928d653a1bcdc83eed580d898a69dd3f122cd1b
mustafa-arkan <arkancse@gmail.com>
2022-10-12 11:23:09 +0600

    header section done

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Homecard from '../components/Homecard/Homecard';
+import logo from '../Images/logo.png'
 
 
 
@@ -17,7 +18,21 @@ const homes=useLoaderData().data
         
         <div>
            
-           <h2>I have home: {homes.length}</h2>
+           <div className='container bg-success p-2 text-dark bg-opacity-25 mb-5 d-flex '>
+           <img  src={logo} alt="" className="src w-25" />
+<div>
+<h1 className='mt-5 me-5'><span className='text-danger'>Q</span>uizzoraa</h1>
+<p className='text-break me-5 '> Quizzora is a basically programming learning website.Motivate every student to mastery with easy-to-customize content combined with tools for inclusive quiz.</p>
+
+
+</div>
+
+
+
+
+
+
+           </div>
 
 {
 
